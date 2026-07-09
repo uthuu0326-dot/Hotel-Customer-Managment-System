75bf35d86a6ac4a184ed3f1631c6cc4ee9062e78
uthuu0326-dot <uthuu0326@gmail.com>
2026-07-09 16:08:14 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+
+def connect_db():
+
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="hotel_db"
+    )
+
+    return db
